4f95430df8dedd5c8fb2f07dee06787e361889f2
melyyasmer <162036096+melyyasmer@users.noreply.github.com>
2024-03-08 15:45:49 +0100

    Add files via upload

---
+++ adr.js
@@ -0,0 +1,113 @@
+const navBarMenu = document.querySelector(".navbar .links");
+const menuBtn = document.querySelector(".menu-btn");
+const hideMenuBtn = navBarMenu.querySelector(".close-btn");
+const showPopupBtn = document.querySelector(".login-btn");
+const fromPopup = document.querySelector(".form-popup");
+const hidePopupBtn = document.querySelector(".form-popup .close-btn");
+const loginLink = document.querySelector(".form-box .bottom-link a");
+const signupLink = document.querySelector(".form-box .bottom-link a");
+
+menuBtn.addEventListener("click", () => {
+    navBarMenu.classList.toggle("show-menu")
+
+});
+
+hideMenuBtn.addEventListener("click",() => menuBtn.click());
+
+showPopupBtn.addEventListener("click",() => {
+    document.body.classList.toggle("show-popup")
+});
+
+hidePopupBtn.addEventListener("click", () => showPopupBtn.click() );
+
+ loginLink.addEventListener("click", () => {
+    fromPopup.classList.toggle(link.id ,"show-signup");
+ });
+ signupLink.addEventListener("click", () => {
+    fromPopup.classList.toggle(link.id ,"show-signup");
+ });
+
+var swiper = new Swiper(".place", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+function toggleDropdown() {
+  var dropdown = document.getElementById("dropdown");
+  if (dropdown.style.display === "none") {
+      dropdown.style.display = "block";
+  } else {
+      dropdown.style.display = "none";
+  }
+}
+const allCross = document.querySelectorAll('.visible-pannel img');
+console.log(allCross);
+allCross.forEach(element => {
+  element.addEventListener('click', function(){
+      const height = this.parentNode.parentNode.childNodes[3].scrollHeight;
+      const currentChoice = this.parentNode.parentNode.childNodes[3];
+     if(this.src.includes('croix')){
+      this.src = 'minus.png';
+      gsap.to(currentChoice, {duration: 0.2, height: height + 40, opacity: 1, padding:'20px 15px'})
+     }else if(this.src.includes('minus')){
+      this.src = 'croix.png';
+      gsap.to(currentChoice, {duration: 0.2, height: 0, opacity: 0, padding:'0px 15px'})
+     }
+  })
+})
+var front = document.querySelector('.face-front');
+var back = document.querySelector('.face-back');
+var flip = document.querySelector('.book-content');
+var uno = document.querySelectorAll('.book');
+var portada = document.querySelectorAll('#portada');
+
+var contZindex = 2;
+var customZindex = 1;
+
+for (var i = 0; i < uno.length; i++) {
+uno[i].style.zIndex = customZindex;
+customZindex--;
+
+uno[i].addEventListener('click', function(e){
+
+  var tgt = e.target;
+  var unoThis = this;
+
+  unoThis.style.zIndex = contZindex;
+  contZindex++;
+
+  if (tgt.getAttribute('class') == 'face-front') {
+    unoThis.style.zIndex = contZindex;
+    contZindex +=20;
+    setTimeout(function(){
+      unoThis.style.transform = 'rotateY(-180deg)';
+    }, 500);
+  }
+  if (tgt.getAttribute("class") == 'face-back') {
+    unoThis.style.zIndex = contZindex;
+    contZindex +=20;
+
+    setTimeout(function(){
+      unoThis.style.transform = 'rotateY(0deg)';
+    }, 500);
+  }
+
+  if (tgt.getAttribute('id') == 'portada') {
+    flip.classList.remove("trnsf-reset");
+    flip.classList.add("trnsf");
+  }
+  if (tgt.getAttribute('id') == 'trsf') {
+    flip.classList.remove("trnsf");
+    flip.classList.add("trnsf-reset");
+  }
+
+});
+}
